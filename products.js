a7cef143d3804129188373c2a061ae326ae2b6eb
Filipeelopess98 <filipee.lopess98@gmail.com>
2024-01-11 15:17:05 -0300

    Update 11/01

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 const options = [
-    { name: 'X-Salada', price: 30, vegan: false, src: "img/x-salada.jpg" },
-    { name: 'X-Bacon', price: 34, vegan: false, src: "img/x-bacon.png" },
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: "img/bacon-egg.png" },
-    { name: 'Monstruoso', price: 50, vegan: false, src: "img/monstruoso.png" },
-    { name: 'Big Vegano', price: 55, vegan: true, src: "img/x-vegan.png" },
-    { name: 'X-Vegan', price: 45, vegan: true, src: "img/monstruoso-vegan.png" },
+    { name: 'X-Salada', ingredient: 'Pão brioche, 2 Hambúrgueres angus 80g, Queijo cheddar, Alface, Tomate, Cebola, Picles e Molho especial', price: 30.00, vegan: false, src: "img/x-salada.jpg" },
+    { name: 'X-Bacon', ingredient: 'Pão brioche com gergelim, Hambúrguer angus 150g, Queijo cheddar, Bacon, Alface, Tomate e Molho especial', price: 34.00, vegan: false, src: "img/x-bacon.png" },
+    { name: 'X-Bacon Egg', ingredient: 'Pão brioche, 2 Hambúrguer angus 150g, Queijo cheddar, Presunto, Bacon, Ovo, Alface, Tomate e Molho especial', price: 39.00, vegan: false, src: "img/bacon-egg.png" },
+    { name: 'Monstruoso', ingredient: 'Pão brioche com gergelim, Hambúrguer angus 200g, Queijo cheddar, Alface, Tomate, Cebola, Picles e Molho especial.', price: 50.00, vegan: false, src: "img/monstruoso.png" },
+    { name: 'Big Vegano', ingredient: 'Pão brioche com gergelim, Cogumelos shitake grelhados , Queijo Vegano de Castanhas e Maionese vegana da casa.', price: 55.00, vegan: true, src: "img/x-vegan.png" },
+    { name: 'X-Vegan', ingredient: 'Pão brioche com grãos, 1 Hambúrguer vegano 150g, Queijo Vegano de Castanha, Alface, Cebola Roxa e Molho vegano de ervas', price: 45.00, vegan: true, src: "img/monstruoso-vegan.png" },
 ]
--- scripts.js
+++ scripts.js
@@ -1,21 +1,58 @@
 const list = document.querySelector('ul');
-const showall = document.querySelector('.showEntireMenu')
-let = myLi = ''
+const buttonShowAll = document.querySelector('.showEntireMenu')
+const buttonDiscoutsAll = document.querySelector('.showDiscounts')
+const buttonTotals = document.querySelector('.showTotal')
+const showVegan = document.querySelector('.showVeganSnacks')
 
-function showAll() {
-    options.forEach(products => {
+
+function showAll(productArray) {
+    let = myLi = ''
+    productArray.forEach((products) => {
         myLi += `
-        <<li>
+        <li>
                 <img src= ${products.src}>
                 <p>${products.name}</p>
-                <p class="price">R$ ${products.price}</p>
+                <p> ${products.ingredient}</p>
+                <p class "price">R$ ${products.price}</p>
         </li>
-    
         `
     });
 
     list.innerHTML = myLi
 }
 
-showall.addEventListener('click', showAll)
+function discountAll() {
+    const newPrice = options.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
+    }))
+    showAll(newPrice)
+}
+
+
+function totalItens() {
+    const sum = options.reduce((acumulator, valueProduct) => {
+        return acumulator + valueProduct.price
+    }, 0)
+    list.innerHTML = `
+    <li>
+        <p> O valor total de todos os itens é R$  ${sum.toFixed(2)}</p>
+    </li>
+    `
+}
+
+
+function veganonly() {
+    const vegan = options.filter((product) => {
+        return product.vegan === true;
+    })
+    showAll(vegan)
+}
+
+
+
+buttonShowAll.addEventListener('click', () => showAll(options))
+buttonDiscoutsAll.addEventListener('click', discountAll)
+buttonTotals.addEventListener('click', totalItens)
+showVegan.addEventListener('click', veganonly)
 
